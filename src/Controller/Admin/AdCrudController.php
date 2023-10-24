811c614f8275375317b42fcb1bb30159c5a73472
KrouriB <brice.krouri@gmail.com>
2023-10-24 15:52:00 +0200

    correction access assosiative part

--- src/Controller/Admin/AdCrudController.php
+++ src/Controller/Admin/AdCrudController.php
@@ -6,6 +6,7 @@ use App\Entity\Ad;
 use App\Form\TagType;
 use App\Repository\AdRepository;
 use App\Repository\TagRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
@@ -40,53 +41,54 @@ class AdCrudController extends AbstractCrudController
             DateField::new('startedAt'),
             DateField::new('endedAt'),
             IntegerField::new('views')->setFormTypeOption('disabled', 'disabled'),
-            // CollectionField::new('tags')
-            //     ->onlyOnForms()
-            //     ->setEntryIsComplex(true) // Indique que chaque entrée est complexe (Module + nombre de jours)
-            //     ->setFormTypeOptions(
-            //         [
-            //         'allow_add' => true, // Autoriser l'ajout de nouvelles entrées
-            //         'allow_delete' => true, // Autoriser la suppression d'entrées existantes
-            //         'by_reference' => false, // Nécessaire pour que les modifications soient persistées correctement
-            //         ]
-            //     )
-            //     ->setEntryType(TagType::class) // Le formulaire pour chaque entrée
-            //     ->setCustomOption('query_builder', function (TagRepository $tagRepository) {
-            //         return $tagRepository->createQueryBuilder('t')
-            //             ->orderBy('t.name', 'ASC');
-            //     })
-            AssociationField::new('tags')
+            CollectionField::new('tags')
+                ->onlyOnForms()
+                ->setEntryIsComplex(true) // Indique que chaque entrée est complexe (Module + nombre de jours)
                 ->setFormTypeOptions(
                     [
-                    'by_reference' => false,
+                    'allow_add' => true, // Autoriser l'ajout de nouvelles entrées
+                    'allow_delete' => true, // Autoriser la suppression d'entrées existantes
+                    'by_reference' => false, // Nécessaire pour que les modifications soient persistées correctement
                     ]
-                ),
+                )
+                ->setEntryType(TagType::class) // Le formulaire pour chaque entrée
+                // ->setCustomOption('query_builder', function (TagRepository $tagRepository) {
+                //     return $tagRepository->createQueryBuilder('t')
+                //         ->orderBy('t.name', 'ASC');
+                // })
+            // AssociationField::new('tags')
+            //     ->setFormTypeOptions(
+            //         [
+            //         'by_reference' => false,
+            //         ]
+            //     ),
         ];
     }
 
     
-
+    #[Route('/admin/tag/select/{id}/', name: 'tag_select')]
     public function tagAds(
         AdRepository $adRepository,
         TagRepository $tagRepository,
+        Request $request
     )
     {
-        $ad = $adRepository->find('id');
-        $notTagged = $tagRepository->findNotTagged('id');
-        $tagged = $tagRepository->findTagged('id');
+        $ad = $adRepository->find($request->get('id'));
+        $notTagged = $tagRepository->findNotTagged($request->get('id'));
+        $tagged = $tagRepository->findTagged($request->get('id'));
         return $this->render('ad/tag.html.twig', [
             'tagged' => $tagged,
             'notTagged' => $notTagged
         ]);
     }
 
-    public function setTagToAds( // potentiellement inutille
-        AdRepository $adRepository,
-        TagRepository $tagRepository,
-        EntityManagerInterface $entityManager
-    )
-    {
-        $ad = $adRepository->find('id');
+    // public function setTagToAds( // potentiellement inutille
+    //     AdRepository $adRepository,
+    //     TagRepository $tagRepository,
+    //     EntityManagerInterface $entityManager
+    // )
+    // {
+    //     $ad = $adRepository->find('id');
         
-    }
+    // }
 }
